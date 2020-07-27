e69854aa95562bb405327c1c7d6a18a3a69442d2
Diego Andrade <45301572+diegoAndrade777@users.noreply.github.com>
2020-07-27 13:58:32 -0300

    Desafio. Personalizar

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é FrontEnd? Trabalhando na área."}
+        videoDescription={"Transformação Pessoal - Copini Series"}
       />
 
       <Carousel
@@ -34,6 +34,16 @@ function App() {
       <Carousel
         ignoreFirstVideo
         category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[5]}
       />
 
       <Footer />
